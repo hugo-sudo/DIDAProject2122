67ae45cd2906fc9f1e1da85fef3c10b9c93e74a9
ist102178 <tomas.guerra@tecnico.ulisboa.pt>
2021-10-25 12:01:01 +0100

    commit auxiliar

--- Scheduler/SchedulerMain.cs
+++ Scheduler/SchedulerMain.cs
@@ -1,31 +1,40 @@
 ﻿using Grpc.Core;
-using Grpc.Net.Client;
 using System;
 
 namespace Scheduler
 {
     class SchedulerMain
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             //recebe server_id e URL do puppetMaster
-            int port = 1001;
-            string url = args[2];
-
+            //int port = 1001;
+            //string url = args[2];
+            const int port = 1001;
+            const string hostname = "localhost";
+            string startupMessage;
             ServerPort serverPort;
-            serverPort = new ServerPort(url, port, ServerCredentials.Insecure);
+
+            serverPort = new ServerPort(hostname, port, ServerCredentials.Insecure);
+            startupMessage = "Insecure ChatServer server listening on port " + port;
+
+
+           // ServerPort serverPort;
+            //serverPort = new ServerPort(url, port, ServerCredentials.Insecure);
 
             Server schedulerServer = new Server
             {
-                Services = { },
+                Services = { DIDASchedulerService.BindService(new SchedulerService()) },
                 Ports = { serverPort }
             };
 
             schedulerServer.Start();
 
+            Console.WriteLine(startupMessage);
+
             //Configuring HTTP for client connections in Register method
             AppContext.SetSwitch(
-    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+       "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
             while (true) ;
 
         }
--- Scheduler/SchedulerService.cs
+++ Scheduler/SchedulerService.cs
@@ -1,14 +1,16 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
+using Structs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Scheduler
 {
-    class SchedulerService : DIDASchedulerService   {
+    class SchedulerService : DIDASchedulerService.DIDASchedulerServiceBase   {
 
         // canal pelo qual os clientes vao comunicar com o scheduler
         private GrpcChannel channel;
@@ -18,12 +20,20 @@ namespace Scheduler
 
         }
 
-        public void receiveClientRequest(clientRequest request)
+        public void receiveClientRequest(DIDARequest request)
         {
-
+            string input = request.input;
+            string app_file = request.application_file;
+            
         }
 
+        public void sendToWorker()
+        {
+            DIDAMetaRecord meta_rec;
+            
+        }
 
+        private DIDARequest request1;
 
 
 
